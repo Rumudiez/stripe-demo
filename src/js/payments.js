8770201c108df07a4c73f48bce2b5c2b1e5cbc50
Taylor Edwards <me@tayloredwards.net>
2017-03-21 21:45:48 -0700

    Working card token retrieval using Striple Elements quickstart: https://stripe.com/docs/elements

--- src/js/payments.js
+++ src/js/payments.js
@@ -1,1 +1,64 @@
-alert('payments.js');
+// Create a Stripe client
+const stripe = Stripe('pk_test_kIlII7rvkl4BZjO2aAbRXbGO');
+
+// Create an instance of Elements
+const elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+const style = {
+  base: {
+    // Add your base input styles here. For example:
+    fontSize: '16px',
+    lineHeight: '24px',
+  },
+  invalid: {
+    color: 'red'
+  }
+};
+
+// Create an instance of the card Element
+const card = elements.create('card', {style});
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', ({error}) => {
+  const displayError = document.getElementById('card-errors');
+  if (error) {
+    displayError.textContent = error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Create a token or display an error the form is submitted.
+const form = document.getElementById('payment-form');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const {token, error} = await stripe.createToken(card);
+
+  if (error) {
+    // Inform the user if there was an error
+    const errorElement = document.getElementById('card-errors');
+    errorElement.textContent = error.message;
+  } else {
+    // Send the token to your server
+    stripeTokenHandler(token);
+  }
+});
+
+const stripeTokenHandler = (token) => {
+  // Insert the token ID into the form so it gets submitted to the server
+  const form = document.getElementById('payment-form');
+  const hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  console.debug('Done!', token);
+  // form.submit();
+}
